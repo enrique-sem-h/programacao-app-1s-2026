5bcf2e3b9c71c9e8a18a1c32968511bd3685700f
enrique-sem-h <carvalhoenrique33@gmail.com>
2026-05-27 09:57:59 -0300

    refactor: fixed warning on jwt `verifyToken` function

--- Backend/infra/jwt/middleware.ts
+++ Backend/infra/jwt/middleware.ts
@@ -9,15 +9,20 @@ export const verifyToken = (
   next: NextFunction,
 ) => {
   const token = req.headers["authorization"]?.split(" ")[1];
+  const secret = process.env.JWT_SECRET;
 
   // verify token presence
   if (!token) {
     return res.status(401).json({ message: "Access denied!" });
   }
 
+  if (!secret) {
+    return res.status(500).json({ error: "Internal err: no secret provided" });
+  }
+
   // verify token validity
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, secret);
     req.user = decoded;
     next();
   } catch (error) {
