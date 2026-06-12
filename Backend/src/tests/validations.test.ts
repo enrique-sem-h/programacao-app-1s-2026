48c1e7b92af8aca5b608957c65e372b1eb0bb428
enrique-sem-h <carvalhoenrique33@gmail.com>
2026-06-12 13:20:01 -0300

    feat: setup test suite

---
+++ Backend/src/tests/validations.test.ts
@@ -0,0 +1,19 @@
+// import Joi from "joi";
+// import test from "node:test";
+// import validate from "root/infra/validations/validations.ts";
+
+// const req: any = {
+// 	body: {},
+// 	file: {},
+// };
+
+// const res: any = {
+// 	status: 0,
+// 	json: {}
+// };
+
+// const next: any = ();
+
+// test("user validation successful should return 200 and user parsed", () => {});
+
+// test("user validation error should return ...", () => {});
